d73d91038f42764169e736b2cb528bf27a1071ea
kevin <kevin@mycause.com.au>
2018-02-26 08:35:21 +1100

    git check

--- src/containers/SurveysList.js
+++ src/containers/SurveysList.js
@@ -1,53 +1,54 @@
-import React, { Component } from "react"
-import axios from "axios"
-import Proptypes from "prop-types"
-import { Link } from "react-router-dom"
+import React, { Component } from 'react'
+import axios from 'axios'
+import Proptypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
 class SurveysList extends Component {
-  static propTypes = {
-    baseResourceUrl: Proptypes.string.isRequired,
-  }
+    static propTypes = {
+        baseResourceUrl: Proptypes.string.isRequired,
+    }
 
-  state = {
-    surveys: [],
-    loading: false,
-    error: false,
-  }
+    state = {
+        surveys: [],
+        loading: false,
+        error: false,
+    }
 
-  componentDidMount() {
-    const { baseResourceUrl } = this.props
-    this.setState({ loading: true })
-    axios
-      .get(baseResourceUrl)
-      .then(({ data }) => {
-        if (data && data.survey_results) {
-          this.setState({ loading: false, surveys: data.survey_results })
-        }
-      })
-      .catch(() => {
-        this.setState({ error: true, loading: false })
-      })
-  }
+    componentDidMount() {
+        const { baseResourceUrl } = this.props
+        this.setState({ loading: true })
+        axios
+            .get(baseResourceUrl)
+            .then(({ data }) => {
+                if (data && data.survey_results) {
+                    this.setState({ loading: false, surveys: data.survey_results })
+                }
+            })
+            .catch(() => {
+                this.setState({ error: true, loading: false })
+            })
+    }
 
-  render() {
-    const { surveys, loading, error } = this.state
-    return (
-      <div className="SurveysListCon">
-        {surveys.map(survey => {
-          // regex to strip string of filetypes
-          const url = survey.url.replace(/\.[^/.]+$/, "")
-          return (
-            <div key={survey.name}>
-              <Link to={url}>{survey.name}</Link>
-              <br />
+    render() {
+        const { surveys, loading, error } = this.state
+        return (
+            <div className="SurveysListCon">
+                {surveys.map(survey => {
+                    // regex to strip string of filetypes
+                    const url = survey.url.replace(/\.[^/.]+$/, '')
+                    return (
+                        <div key={survey.name}>
+                            <Link to={url}>{survey.name}</Link>
+                            <br />
+                        </div>
+                    )
+                })}
+                git check
+                {loading && 'loading now'}
+                {error && 'could not get data from server please refresh moment later'}
             </div>
-          )
-        })}
-        {loading && "loading now"}
-        {error && "could not get data from server please refresh moment later"}
-      </div>
-    )
-  }
+        )
+    }
 }
 
 export default SurveysList
