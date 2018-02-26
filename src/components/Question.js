f6281785cd90f79613b20b33733b496cbf39cc40
Kevin <tig3r86@gmail.com>
2018-02-26 19:31:53 +1100

    finish the arimetic part for average rating

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,14 +1,24 @@
-import React from 'react'
-import Proptypes from 'prop-types'
+import React from "react"
+import Proptypes from "prop-types"
+
+function precisionRound(number, precision) {
+  const factor = 10 ** precision
+  return Math.round(number * factor) / factor
+}
 
 export default function Question({ description, surveyResponse }) {
-    console.log(description, surveyResponse)
-    let avgRating = surveyResponse.reduce()
-    return (
-        <div className="QuestionCon">
-            <p>{description}</p>
-        </div>
-    )
+  const ratingTotal = surveyResponse.reduce((accu, currentVal) => {
+    const rating = currentVal.response_content && parseInt(currentVal.response_content, 10)
+    return accu + rating
+  }, 0)
+  const averageRating = precisionRound(ratingTotal / surveyResponse.length, 2)
+
+  return (
+    <div className="QuestionCon">
+      <p>{description}</p>
+      <p>{averageRating}</p>
+    </div>
+  )
 }
 
 //Question.propTypes = {
